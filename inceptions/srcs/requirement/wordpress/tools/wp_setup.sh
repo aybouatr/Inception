db12df03a6e11ab0c37c8411226859fd93fd07ba
Ayoub Bouatrouss <aybouatr@c4r4p10.1337.ma>
2026-07-12 15:01:49 +0100

    add

--- inceptions/srcs/requirement/wordpress/tools/wp_setup.sh
+++ inceptions/srcs/requirement/wordpress/tools/wp_setup.sh
@@ -1,17 +1,66 @@
 #!/bin/sh
+
 set -e
 
-echo "Starting WordPress container..."
-
-cd /var/www/html
+echo "Starting WordPress..."
 
 mkdir -p /run/php
 
-# If WordPress is not downloaded, download it
+cd /var/www/html
+
+echo "Waiting for MariaDB..."
+
+until mysqladmin ping \
+    -h"$MYSQL_HOST" \
+    -u"$MYSQL_USER" \
+    -p"$MYSQL_PASSWORD" \
+    --silent
+do
+    sleep 2
+done
+
+echo "MariaDB is ready."
+
 if [ ! -f wp-load.php ]; then
     echo "Downloading WordPress..."
-    php -d memory_limit=512M /usr/local/bin/wp core download --allow-root
+
+    wp core download \
+        --allow-root
 fi
 
-echo "WordPress ready. Starting PHP-FPM..."
+if [ ! -f wp-config.php ]; then
+    echo "Creating wp-config.php..."
+
+    wp config create \
+        --dbname="$MYSQL_DATABASE" \
+        --dbuser="$MYSQL_USER" \
+        --dbpass="$MYSQL_PASSWORD" \
+        --dbhost="$MYSQL_HOST" \
+        --allow-root
+fi
+
+if ! wp core is-installed --allow-root
+then
+    echo "Installing WordPress..."
+
+    wp core install \
+        --url="$DOMAIN_NAME" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --allow-root
+
+    echo "Creating normal user..."
+
+    wp user create \
+        "$WP_USER" \
+        "$WP_USER_EMAIL" \
+        --user_pass="$WP_USER_PASSWORD" \
+        --role=author \
+        --allow-root
+fi
+
+echo "Starting PHP-FPM..."
+
 exec php-fpm -F
